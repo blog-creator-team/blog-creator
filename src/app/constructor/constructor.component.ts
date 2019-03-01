b4b3f19c88876b6c971a848285b1526710defa84
v-lavs <lavskavanda@gmail.com>
2019-03-01 23:37:26 +0200

    class transfer container to a separate component

--- src/app/constructor/constructor.component.ts
+++ src/app/constructor/constructor.component.ts
@@ -23,33 +23,33 @@ export class ConstructorComponent {
   }
 
   /*----------    added containers    ----------*/
-  addContainer(index: number, type: string) {
-    const container = new Container(CONTAINER_DEFAULT_SETTINGS);
-
-    if (index === this.containers.length) {
-      this.containers.push(container);
-    } else if (type === 'prepend') {
-      const updatedData = [];
-      this.containers.forEach((item, itemIndex) => {
-        if (itemIndex === index) {
-          updatedData.push(container, item)
-        } else {
-          updatedData.push(item)
-        }
-      });
-      this.containers = updatedData;
-    } else if (type === 'append') {
-      const updatedData = [];
-      this.containers.forEach((item, itemIndex) => {
-        if (itemIndex === index) {
-          updatedData.push(item, container)
-        } else {
-          updatedData.push(item)
-        }
-      });
-      this.containers = updatedData;
-    }
-  }
+  // addContainer(index: number, type: string) {
+  //   const container = new Container(CONTAINER_DEFAULT_SETTINGS);
+  //
+  //   if (index === this.containers.length) {
+  //     this.containers.push(container);
+  //   } else if (type === 'prepend') {
+  //     const updatedData = [];
+  //     this.containers.forEach((item, itemIndex) => {
+  //       if (itemIndex === index) {
+  //         updatedData.push(container, item)
+  //       } else {
+  //         updatedData.push(item)
+  //       }
+  //     });
+  //     this.containers = updatedData;
+  //   } else if (type === 'append') {
+  //     const updatedData = [];
+  //     this.containers.forEach((item, itemIndex) => {
+  //       if (itemIndex === index) {
+  //         updatedData.push(item, container)
+  //       } else {
+  //         updatedData.push(item)
+  //       }
+  //     });
+  //     this.containers = updatedData;
+  //   }
+  // }
   /*-------------------------------------------*/
 
   // showSidebar() {
